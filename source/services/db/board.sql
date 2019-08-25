ad4cf6903184f3aa751d1ff5832cf2e0745dea9f
Zhiyuan Wang <wangzyzju@gmail.com>
2019-08-24 22:36:41 -0400

    Use text type instead of long varchars

--- source/services/db/board.sql
+++ source/services/db/board.sql
@@ -1,7 +1,7 @@
 CREATE TABLE board (
 	board_id	SERIAL		PRIMARY KEY,
 	board_name	VARCHAR (128)	NOT NULL,
-	board_note	VARCHAR (1024),
+	board_note	TEXT,
 	board_type	INTEGER		NOT NULL, -- personal or team, only one of user_id or team_id will be used
 	user_id		INTEGER 	REFERENCES users(user_id) ON DELETE CASCADE,
 	team_id		INTEGER		REFERENCES team(team_id) ON DELETE CASCADE,
--- source/services/db/story.sql
+++ source/services/db/story.sql
@@ -9,6 +9,6 @@ create TABLE story (
 	epic_id		INTEGER		REFERENCES epic(epic_id) ON DELETE RESTRICT,
 	assignee	INTEGER		REFERENCES users(user_id),
 	reporter	INTEGER		REFERENCES users(user_id),
-	description	VARCHAR (1024),
+	description	TEXT,
 	parent_story	INTEGER		REFERENCES story(story_id)
 );
