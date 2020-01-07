38c4a53a09265c9ddc5f3d6d715d2f73fe756392
Zhiyuan Wang <wangzyzju@gmail.com>
2020-01-07 01:09:03 -0500

    Create project

--- source/db/schema/project.sql
+++ source/db/schema/project.sql
@@ -2,7 +2,7 @@ CREATE TABLE project (
 	project_id	SERIAL		PRIMARY KEY,
 	project_name	varchar (128)	NOT NULL,
 	project_note	text,
-	project_type	integer		NOT NULL, -- personal or team, only one of user_id or team_id will be used
+	project_type	varchar (16)	NOT NULL, -- 'personal' or 'team', only one of user_id or team_id will be used
 	project_public	boolean		NOT NULL, -- public project or private project
 	user_id		integer		REFERENCES users(user_id) ON DELETE CASCADE,
 	team_id		integer		REFERENCES team(team_id) ON DELETE CASCADE,
