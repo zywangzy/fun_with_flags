79acefa9c110c95e8a3c81f56b258e1d0b8a72ad
Zhiyuan Wang <wangzyzju@gmail.com>
2020-03-18 22:35:15 -0400

    Add logout integration test

--- source/funwithflags/use_cases/refresh_access_token.py
+++ source/funwithflags/use_cases/refresh_access_token.py
@@ -7,6 +7,8 @@ from funwithflags.gateways.context import Context
 
 
 def refresh_access_token(identity: Any, context: Context) -> str:
+    """Give refresh token, refresh the user (identity is user_id) login status and return a new access token.
+    """
     new_token = flask_jwt_extended.create_access_token(identity=identity)
     context.redis_gateway.set(flask_jwt_extended.get_jti(encoded_token=new_token), "login")
     return new_token
--- source/integration_tests/test_integration_logout.py
+++ source/integration_tests/test_integration_logout.py
@@ -1,6 +1,27 @@
 """Integration test for logout."""
 import pytest
 
+import flask_jwt_extended
 
-def test_logout():
-    pass
+from funwithflags.definitions import LogoutRequest
+from funwithflags.use_cases import logout
+
+
+@pytest.mark.usefixtures("context")
+def test_logout(monkeypatch, context):
+    # Given
+    refresh_token = "token"
+    expected_jti = "fake.jti"
+    expected_status = "logout"
+
+    def mock_get_jti(encoded_token):
+        return expected_jti
+
+    monkeypatch.setattr(flask_jwt_extended, 'get_jti', mock_get_jti)
+
+    # When
+    logout(LogoutRequest(1, refresh_token), context)
+
+    # Then
+    status = context.redis_gateway.get(expected_jti)
+    assert status and expected_status == status
