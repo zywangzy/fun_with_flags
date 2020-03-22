f5448829566c50e0d7358af47cd16cbff20e93f6
Zhiyuan Wang <wangzyzju@gmail.com>
2020-03-22 17:17:27 -0400

    Fix swagger apidocs routing

--- source/funwithflags/app.py
+++ source/funwithflags/app.py
@@ -51,7 +51,22 @@ app.config['SWAGGER'] = {
     }
 }
 
-swagger = Swagger(app)
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec_1",
+            "route": "/api/apispec_1.json",
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/api/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/api/apidocs/"
+}
+
+swagger = Swagger(app, config=swagger_config)
 
 
 UNSUPPORTED = "Unsupported function"
